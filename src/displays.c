e2d82ea3a8f3d0c4f72629437b60740e6992714e
Natalia Arosemena <nataliaalmillategui@gmail.com>
2026-04-11 22:18:33 -0400

    Update displays.c

--- src/displays.c
+++ src/displays.c
@@ -1,1 +1,15 @@
+//WELCOME SCREEN AKA IDLE STATE
+void DRAW_WELCOME(Graphics_Context *context){
+  Graphics_clearDisplay(context);
+    // Set background and foreground colors
+    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_PINK);
 
+    // Set the default font for strings
+    GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+    // Print message
+    Graphics_drawStringCentered(&g_sContext, "Tone Tracker", AUTO_STRING_LENGTH, 64, 10, OPAQUE_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "Made By:", AUTO_STRING_LENGTH, 64, 105, OPAQUE_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "Natalia & Benjamin", AUTO_STRING_LENGTH, 64, 120, OPAQUE_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "PRESS S1 TO PLAY", AUTO_STRING_LENGTH, 64, 50, OPAQUE_TEXT);
+}
