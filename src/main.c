a9ba9dd8ccf13de6e759236d7468d35c8be69700
Natalia Arosemena <nataliaalmillategui@gmail.com>
2026-04-16 18:23:11 -0400

    Add system state enumeration and update main loop
    Added a new system state enumeration and updated the main loop to handle different states.

--- src/main.c
+++ src/main.c
@@ -10,11 +10,18 @@
 
 void Initialize_Clock_System();
 void DRAW_WELCOME(Graphics_Context *context);
+typedef enum {
+    STATE_IDLE,
+    STATE_STARTUP
+    STATE_MENU,
+    STATE_TRACKING
+} system_state_t;
 //global variables
+Graphics_Context g_sContext;
 //MENU STATE
+volatile system_state_t current_state = STATE_WELCOME;
 volatile unsigned int menu_rounds = 1;
-volatile unsigned char menu_needs_REDRAW = 1;
-
+volatile unsigned char menu_needs_redraw = 1;
 
 void main(void) {
     // Stop the watchdog timer (critical for all MSP430 code)
@@ -32,15 +39,14 @@ void main(void) {
 
     TA1CCR1 = 1136;       
     
-
+//WE SHOULD CONTROL THE BUZZER IN STATE BEHAVIOR, we dont want tone to start right away and stay on
     TA1CCTL1 = OUTMOD_7;
     TA1CTL = TASSEL_2 | MC_1 | TACLR;
 
-    Graphics_Context g_sContext;
+    
     //INIT PIXEL DISPLAY
     Crystalfontz128x128_Init();
     Crystalfontz128x128_SetOrientation(0);
-
     Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128);
     Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
     Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
@@ -51,6 +57,25 @@ void main(void) {
 
     // Infinite loop to keep the microcontroller running
     while (1) {
+       switch (current_state)
+        {
+        case STATE_WELCOME:
+            // Wait for button or timeout to move to menu
+            break;
+
+        case STATE_MENU:
+            if (menu_needs_redraw)
+            {
+                menu_needs_redraw = 0;
+                DRAW_MENU(&g_sContext);
+            }
+            break;
+
+        case STATE_TRACKING:
+            break;
+
+        default:
+            break;
         // Just hanging out while the buzzer screams
     }
 }
