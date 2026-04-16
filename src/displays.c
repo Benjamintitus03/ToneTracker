81cf226d535295bf58294a63bd182e0d11ef74b6
Natalia Arosemena <nataliaalmillategui@gmail.com>
2026-04-16 17:40:29 -0400

    Refactor welcome screen functions and add startup display
    Renamed DRAW_WELCOME to display_idle and added display_startup function for startup screen.

--- src/displays.c
+++ src/displays.c
@@ -1,5 +1,5 @@
 //WELCOME SCREEN AKA IDLE STATE
-void DRAW_WELCOME(Graphics_Context *context){
+void display_idle(Graphics_Context *context){
   Graphics_clearDisplay(context);
     // Set background and foreground colors
     Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
@@ -13,3 +13,18 @@ void DRAW_WELCOME(Graphics_Context *context){
     Graphics_drawStringCentered(&g_sContext, "Natalia & Benjamin", AUTO_STRING_LENGTH, 64, 120, OPAQUE_TEXT);
     Graphics_drawStringCentered(&g_sContext, "PRESS S1 TO PLAY", AUTO_STRING_LENGTH, 64, 50, OPAQUE_TEXT);
 }
+// MUSIC PLAYS FOR 5 SECONDS IN THIS STATE
+void display_startup(Graphics_Context *context){
+  Graphics_clearDisplay(context);
+    // Set background and foreground colors
+    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_PINK);
+
+    // Set the default font for strings
+    GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+    // Print message
+    Graphics_drawStringCentered(&g_sContext, "Tone Tracker", AUTO_STRING_LENGTH, 64, 10, OPAQUE_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "Made By:", AUTO_STRING_LENGTH, 64, 105, OPAQUE_TEXT);
+    Graphics_drawStringCentered(&g_sContext, "Natalia & Benjamin", AUTO_STRING_LENGTH, 64, 120, OPAQUE_TEXT);=
+}
+
